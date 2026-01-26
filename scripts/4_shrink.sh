ab824710b49f84ffc5ab6a2d78c33cff47bf4d9a
Martbul <martbul01@gmail.com>
2026-01-26 23:35:40 +0200

    fix: remove pishrink

--- scripts/4_shrink.sh
+++ scripts/4_shrink.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-# INPUT: The large working image we created in step 1
 IMAGE_FILE="../work_image.img"
-# OUTPUT: The final compressed release file
 OUTPUT_FILE="../strct-release-v1.img"
 MOUNT_POINT="mnt_root"
 
@@ -19,18 +17,8 @@ umount -lf $MOUNT_POINT || true
 # Detach loop device
 losetup -D
 
-echo "Shrinking image using PiShrink..."
+echo "Finalizing image..."
 
-if [ ! -f "pishrink.sh" ]; then
-    wget https://raw.githubusercontent.com/Drewsif/PiShrink/master/pishrink.sh
-    chmod +x pishrink.sh
-fi
-
-# Run PiShrink
-# Arguments: [Input File] [Output File]
-./pishrink.sh "$IMAGE_FILE" "$OUTPUT_FILE"
-
-# Delete the large working file to save space
-rm "$IMAGE_FILE"
+mv "$IMAGE_FILE" "$OUTPUT_FILE"
 
 echo "[OK] DONE! Final image is located at: $OUTPUT_FILE"
